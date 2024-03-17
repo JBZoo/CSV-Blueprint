27ba8e7883437c7de4a6938e9aa8dccbf048e9db
Denis Smetannikov <SmetDenis@users.noreply.github.com>
2024-03-17 23:40:37 +0400

    Add `count_empty` function and update examples (#40)
    Introduced count_empty function to count empty values in CSV files. This
    new feature has been reflected in all example files including full.json,
    full.php, and full.yml. Additionally, created accompanying
    ComboCountEmpty class and test file to ensure accurate implementation
    and functioning of this feature.

---
+++ tests/Rules/Aggregate/ComboCountEmptyTest.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * JBZoo Toolbox - Csv-Blueprint.
+ *
+ * This file is part of the JBZoo Toolbox project.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT
+ * @copyright  Copyright (C) JBZoo.com, All rights reserved.
+ * @see        https://github.com/JBZoo/Csv-Blueprint
+ */
+
+declare(strict_types=1);
+
+namespace JBZoo\PHPUnit\Rules\Aggregate;
+
+use JBZoo\CsvBlueprint\Rules\AbstarctRule as Combo;
+use JBZoo\CsvBlueprint\Rules\Aggregate\ComboCountEmpty;
+use JBZoo\PHPUnit\Rules\AbstractAggregateRuleCombo;
+
+use function JBZoo\PHPUnit\isSame;
+
+class ComboCountEmptyTest extends AbstractAggregateRuleCombo
+{
+    protected string $ruleClass = ComboCountEmpty::class;
+
+    public function testEqual(): void
+    {
+        $rule = $this->create(3, Combo::EQ);
+
+        isSame('', $rule->test(['', '', '']));
+        isSame('', $rule->test(['', '', '', '1']));
+        isSame('', $rule->test(['', '', '', ' ', '1']));
+
+        isSame(
+            'The number of empty rows in the column is "2", which is not equal than the expected "3"',
+            $rule->test(['', '', ' ', '1']),
+        );
+    }
+
+    public function testNotEqual(): void
+    {
+        $rule = $this->create(3, Combo::NOT);
+
+        isSame('', $rule->test(['', '']));
+
+        isSame(
+            'The number of empty rows in the column is "3", which is equal than the not expected "3"',
+            $rule->test(['', '', '']),
+        );
+    }
+
+    public function testMin(): void
+    {
+        $rule = $this->create(3, Combo::MIN);
+
+        isSame('', $rule->test(['', '', '']));
+        isSame('', $rule->test(['', '', '', '1']));
+
+        isSame(
+            'The number of empty rows in the column is "2", which is less than the expected "3"',
+            $rule->test(['', '', ' ']),
+        );
+    }
+
+    public function testMax(): void
+    {
+        $rule = $this->create(3, Combo::MAX);
+
+        isSame('', $rule->test(['', '', '']));
+        isSame('', $rule->test(['', '']));
+        isSame('', $rule->test([]));
+
+        isSame(
+            'The number of empty rows in the column is "4", which is greater than the expected "3"',
+            $rule->test(['', '', '', '']),
+        );
+    }
+
+    public function testInvalidOption(): void
+    {
+        $this->expectExceptionMessage(
+            'Invalid option "[1, 2]" for the "ag:count_empty_max" rule. It should be integer/float.',
+        );
+        $rule = $this->create([1, 2], Combo::MAX);
+        $rule->validate(['1', '2', '3']);
+    }
+
+    public function testInvalidParsing(): void
+    {
+        $rule = $this->create(0, Combo::EQ);
+        isSame('', $rule->test([]));
+    }
+}
