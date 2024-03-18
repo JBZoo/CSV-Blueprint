ad9d8aa10b4190f00f2117ba48381063cf2c7a91
Denis Smet <denis.smetannikov@yandex.ru>
2024-03-18 04:10:08 +0400

    Refactor standard deviation to include sample and population
    Standard deviation parameters in full.json, full.php, full.yml and README.md files were renamed for clarity. They now include both sample and population standard deviation. Ensued that this change is correctly reflected in related code and related tests have been added for standard deviation of a population.

---
+++ src/Rules/Aggregate/ComboSdPopulation.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * JBZoo Toolbox - Csv-Blueprint.
+ *
+ * This file is part of the JBZoo Toolbox project.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT
+ * @copyright  Copyright (C) JBZoo.com, All rights reserved.
+ * @see        https://github.com/JBZoo/Csv-Blueprint
+ */
+
+declare(strict_types=1);
+
+namespace JBZoo\CsvBlueprint\Rules\Aggregate;
+
+use MathPHP\Statistics\Descriptive;
+
+final class ComboSdPopulation extends AbstarctAggregateRuleCombo
+{
+    protected const NAME = 'standard deviation (SD+)';
+
+    protected const HELP_TOP = [
+        'SD+ (Standard deviation for a population; uses population variance)',
+    ];
+
+    protected function getActualAggregate(array $colValues): ?float
+    {
+        return Descriptive::standardDeviation(self::stringsToFloat($colValues), Descriptive::POPULATION);
+    }
+}
---
+++ tests/Rules/Aggregate/ComboSdPopulationTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * JBZoo Toolbox - Csv-Blueprint.
+ *
+ * This file is part of the JBZoo Toolbox project.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT
+ * @copyright  Copyright (C) JBZoo.com, All rights reserved.
+ * @see        https://github.com/JBZoo/Csv-Blueprint
+ */
+
+declare(strict_types=1);
+
+namespace JBZoo\PHPUnit\Rules\Aggregate;
+
+use JBZoo\CsvBlueprint\Rules\AbstarctRule as Combo;
+use JBZoo\CsvBlueprint\Rules\Aggregate\ComboSdPopulation;
+use JBZoo\PHPUnit\Rules\AbstractAggregateRuleCombo;
+
+use function JBZoo\PHPUnit\isSame;
+
+class ComboSdPopulationTest extends AbstractAggregateRuleCombo
+{
+    protected string $ruleClass = ComboSdPopulation::class;
+
+    public function testEqual(): void
+    {
+        $rule = $this->create(2.3570226039552, Combo::EQ);
+        isSame('', $rule->test([1, 5, 1, 1, 1, 2, 8, 1, 1]));
+
+        $rule = $this->create(3, Combo::EQ);
+        isSame(
+            'The standard deviation (SD+) in the column is "2.3570226039552", which is not equal than the expected "3"',
+            $rule->test([1, 5, 1, 1, 1, 2, 8, 1, 1]),
+        );
+    }
+}
