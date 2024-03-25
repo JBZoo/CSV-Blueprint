d8939a43a11742df85f95a3b2a24ec70f376e5e7
SmetDenis <smet.denis@gmail.com>
2024-03-26 02:54:02 +0400

    Add cubic mean rule and tests
    This commit introduces the cubic mean rule into the schema with corresponding definitions. A new PHP class file has been created for the implementation of this rule and respective unit tests have been created to check its correctness.

---
+++ src/Rules/Aggregate/ComboCubicMean.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * JBZoo Toolbox - Csv-Blueprint.
+ *
+ * This file is part of the JBZoo Toolbox project.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT
+ * @copyright  Copyright (C) JBZoo.com, All rights reserved.
+ * @see        https://github.com/JBZoo/Csv-Blueprint
+ */
+
+declare(strict_types=1);
+
+namespace JBZoo\CsvBlueprint\Rules\Aggregate;
+
+use JBZoo\CsvBlueprint\Rules\AbstarctRule;
+use MathPHP\Statistics\Average;
+
+final class ComboCubicMean extends AbstarctAggregateRuleCombo
+{
+    public const INPUT_TYPE = AbstarctRule::INPUT_TYPE_FLOATS;
+
+    protected const NAME = 'cubic mean';
+
+    public function getHelpMeta(): array
+    {
+        return [['Cubic mean. See: https://en.wikipedia.org/wiki/Cubic_mean'], []];
+    }
+
+    protected function getActualAggregate(array $colValues): ?float
+    {
+        if (\count($colValues) === 0) {
+            return null;
+        }
+
+        return Average::cubicMean(self::stringsToFloat($colValues));
+    }
+}
---
+++ tests/Rules/Aggregate/ComboCubicMeanTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * JBZoo Toolbox - Csv-Blueprint.
+ *
+ * This file is part of the JBZoo Toolbox project.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT
+ * @copyright  Copyright (C) JBZoo.com, All rights reserved.
+ * @see        https://github.com/JBZoo/Csv-Blueprint
+ */
+
+declare(strict_types=1);
+
+namespace JBZoo\PHPUnit\Rules\Aggregate;
+
+use JBZoo\CsvBlueprint\Rules\AbstarctRule as Combo;
+use JBZoo\CsvBlueprint\Rules\Aggregate\ComboCubicMean;
+use JBZoo\PHPUnit\Rules\TestAbstractAggregateRuleCombo;
+
+use function JBZoo\PHPUnit\isSame;
+
+class ComboCubicMeanTest extends TestAbstractAggregateRuleCombo
+{
+    protected string $ruleClass = ComboCubicMean::class;
+
+    public function testEqual(): void
+    {
+        $rule = $this->create(15.492307432707, Combo::EQ);
+        isSame('', $rule->test([]));
+        isSame('', $rule->test([13, 18, 13, 14, 13, 16, 14, 21, 13]));
+
+        $rule = $this->create(1, Combo::EQ);
+        isSame(
+            'The cubic mean in the column is "15.492307432707", which is not equal than the expected "1"',
+            $rule->test([13, 18, 13, 14, 13, 16, 14, 21, 13]),
+        );
+    }
+}
