8ca0a87473cbae76f683af75525eafa9da60e261
SmetDenis <smet.denis@gmail.com>
2024-03-29 15:21:11 +0400

    Add type conversion for aggregate rules
    Modified AbstractAggregateRuleCombo to support type conversion using array_map. Provided specific conversion implementations for float and integer input types, which casts the values in the "colValues" array to the appropriate format.

--- src/Rules/Aggregate/AbstractAggregateRuleCombo.php
+++ src/Rules/Aggregate/AbstractAggregateRuleCombo.php
@@ -53,6 +53,15 @@ abstract class AbstractAggregateRuleCombo extends AbstarctRuleCombo
         $name = static::NAME;
 
         try {
+            // TODO: Think about the performance optimization here
+            if (static::INPUT_TYPE === AbstarctRule::INPUT_TYPE_FLOATS) {
+                $colValues = \array_map('floatval', $colValues);
+            }
+
+            if (static::INPUT_TYPE === AbstarctRule::INPUT_TYPE_INTS) {
+                $colValues = \array_map('intval', $colValues);
+            }
+
             $actual = $this->getActualAggregate($colValues); // Important to use the original method here!
         } catch (\Throwable $exception) {
             return $exception->getMessage(); // TODO: Expose the error/warning message in the report?
