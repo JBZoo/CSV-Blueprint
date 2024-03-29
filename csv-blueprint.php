509e88cc40df2126768429fc47282265c964a3bc
SmetDenis <smet.denis@gmail.com>
2024-03-29 22:08:57 +0400

    Refactor configuration and documentation for 'extra' options
    The 'ansi' argument is now nested under the 'extra' options for improved customization. Hardcoded values in action.yml have been replaced to allow users to adjust verbosity levels and output formats more freely. Changes have also been reflected in the README.md for user guidance.

--- csv-blueprint.php
+++ csv-blueprint.php
@@ -16,25 +16,12 @@ declare(strict_types=1);
 
 namespace JBZoo\CsvBlueprint;
 
+$_SERVER['argv'] = \array_map('trim', $_SERVER['argv']); // Fix for GitHub actions. See action.yml
+
 \define('PATH_ROOT', __DIR__);
-
-$vendorPaths = [
-    __DIR__ . '/../../autoload.php',
-    __DIR__ . '/../vendor/autoload.php',
-    __DIR__ . '/vendor/autoload.php',
-];
-
-foreach ($vendorPaths as $file) {
-    if (\file_exists($file)) {
-        \define('JBZOO_AUTOLOAD_FILE', $file);
-        break;
-    }
-}
-
-require_once JBZOO_AUTOLOAD_FILE;
+require_once __DIR__ . '/vendor/autoload.php';
 
 \date_default_timezone_set('UTC');
-
 \set_error_handler(static function ($severity, $message, $file, $line): void {
     throw new \ErrorException($message, 0, $severity, $file, $line);
 });
