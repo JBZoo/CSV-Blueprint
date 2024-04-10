f0754a07f9046014bff4a36598dde12e4b026927
SmetDenis <smet.denis@gmail.com>
2024-04-10 22:35:12 +0400

    Reorder PHP_SAPI checks in csv-blueprint.php
    The PHP_SAPI checks have been reorganized to avoid redundancy. By consolidating the code, the script's instructions have been streamlined and readability has been improved.

--- csv-blueprint.php
+++ csv-blueprint.php
@@ -18,19 +18,15 @@ namespace JBZoo\CsvBlueprint;
 
 use JBZoo\CsvBlueprint\Workers\WorkerPool;
 
-if ('cli' !== \PHP_SAPI) {
-    throw new \RuntimeException('This script must be run from the command line.');
-}
-
 \define('PATH_ROOT', __DIR__);
 $autoloader = __DIR__ . '/vendor/autoload.php';
 require_once $autoloader;
 
-WorkerPool::setBootstrap($autoloader);
-
 if ('cli' !== \PHP_SAPI) {
-    throw new Exception('This script must be run from the command line.');
+    throw new \RuntimeException('This script must be run from the command line.');
 }
+
+WorkerPool::setBootstrap($autoloader);
 
 // Fix for GitHub actions. See action.yml
 $_SERVER['argv'] = Utils::fixArgv($_SERVER['argv'] ?? []);
